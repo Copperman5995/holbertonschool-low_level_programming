e47cdb9e1321e02bd32474187a9f3ef2098d9c72
root <cooper.d.mix@gmail.com>
2023-01-25 08:37:11 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
 #include<string.h>
 int main(void)
 {
-  printf("with propper grammar, but the outcome is a piece of art,");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
