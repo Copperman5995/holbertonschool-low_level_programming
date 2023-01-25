c26fec6f87a949bf5f4065bcd7857e3cc432cd76
root <cooper.d.mix@gmail.com>
2023-01-25 08:36:06 -0800

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ *main - print out a string using the printf command
+ *Return: main returns 0
+ */
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+  printf("with propper grammar, but the outcome is a piece of art,");
+  return (0);
+}
