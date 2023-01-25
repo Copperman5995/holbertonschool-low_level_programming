4ee474f7decc3194e059137ed056bbd1ddff2c03
root <cooper.d.mix@gmail.com>
2023-01-25 08:23:07 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 #include<string.h>
 int main(void)
 {
-char Mystr[] = "Programming is like building a multilingual puzzle\n";
+char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
 puts(Mystr);
 return (0);
 }
