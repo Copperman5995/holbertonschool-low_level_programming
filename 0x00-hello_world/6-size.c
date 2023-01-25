f180b9c96a482490260bbc6494301e4e20199add
root <cooper.d.mix@gmail.com>
2023-01-25 10:08:32 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ long long int llSize;
 printf("Size of a char: %d byte(s)\n", sizeof(charSize));
 printf("Size of an int: %d byte(s)\n", sizeof(intSize));
 printf("Size of a long int: %ld byte(s)\n", sizeof(longSize));
-printf("Size of a long long int: %lld byte(s)\n", sizeof(llSize));
+printf("Size of a long long int: %lli byte(s)\n", sizeof(llSize));
 printf("Size of a float: %d byte(s)\n", sizeof(floatSize));
 return (0);
 }
