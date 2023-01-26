1db8f45702d2cabe03f53d2795b236978c244cd8
root <cooper.d.mix@gmail.com>
2023-01-26 10:41:46 -0800

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,15 @@
 /**
-* main - dsgadfgadfgdgdfd
-* Return: returns 0
+*main - dsgadfgadfgdgdfd
+*Return: returns 0
 */
 #include<stdio.h>
-#include<string.h>
 int main(void)
 {
 char ch;
-for (ch = 'a'; ch <= 'z': ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
-putchar("\n")
+putchar('\n');
 return (0);
 } 
