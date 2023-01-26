a8c6544a7da3f3c296701c04175d6b1b065086d8
root <cooper.d.mix@gmail.com>
2023-01-26 10:57:38 -0800

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+/**
+*main - dsgadfgadfgdgdfd
+*Return: returns 0
+*/
+#include<stdio.h>
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' || ch != 'q' )
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
