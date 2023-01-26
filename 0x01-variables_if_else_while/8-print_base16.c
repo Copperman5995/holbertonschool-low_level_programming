f153397b908c013dfa715955c6f6b6f91ff899a1
root <cooper.d.mix@gmail.com>
2023-01-26 11:59:48 -0800

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+/**
+*main - dsgadfgadfgdgdfd
+*Return: returns 0
+*/
+#include<stdio.h>
+int main(void)
+{
+int p;
+ char ch;
+for (p = 0; p < 10; p++)
+{
+putchar('0' + p);
+}
+for (ch = 'a'; p < 10; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
