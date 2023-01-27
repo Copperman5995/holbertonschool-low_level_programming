871b267b53b665d700bb893c80cbbc78617fa650
root <cooper.d.mix@gmail.com>
2023-01-27 09:50:39 -0800

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+/**
+ * main - jbjzfm,kjdfskjEWNKJDSTBJOIGBJKL
+ * Return: returns 0
+ */
+#include"main.h"
+int main (void)
+{
+	int ab[28] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n' };
+	int i = 0;
+	while (i < 27)
+	{
+		putchar(ab[i]);
+		i++;
+	}
+	return (0);
+}
