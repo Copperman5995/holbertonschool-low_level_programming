9daa91a6372376d9babcfc9e1ce6642788e46f33
root <cooper.d.mix@gmail.com>
2023-01-27 11:48:27 -0800

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,11 @@
 #include"main.h"
 void print_alphabet(void)
 {
-	char ab[28] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n' };
-	int i = 0;
-	while (i < 27)
-	{
-		_putchar(ab[i]);
-		i++;
-	}
-	return (0);
+char ab = 'a';
+while (ab <= 'z')
+{
+_putchar(ab);
+ab++;
+}
+return (0);
 }
