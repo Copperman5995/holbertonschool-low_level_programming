507fb8197e6b6b55abfaf14aadbbbaef1c40569f
root <cooper.d.mix@gmail.com>
2023-01-27 16:00:40 -0800

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+/**
+ * print_alphabet - jbjzfm,kjdfskjEWNKJDSTBJOIGBJKL
+ * Return: returns 0
+ */
+#include"main.h"
+void print_alphabet_x10(void)
+{
+char ab = 'a';
+for (i = 0; i <= 10; i++)
+{
+while (ab <= 'z')
+{
+_putchar(ab);
+ab++;
+}
+ab = 'a';
+_putchar('\n');
+}
+}
