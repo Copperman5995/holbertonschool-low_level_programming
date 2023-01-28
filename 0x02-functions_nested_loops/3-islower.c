0f6ea7497dd0dcb8e71156168adbf3bb74691632
root <cooper.d.mix@gmail.com>
2023-01-27 16:24:10 -0800

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+/**
+ * main - dfhjgdsbjfjdfnjgjubh
+ * Return: return 1 if loercase 0 if not
+ */
+#include "main.h"
+int _islower(int c)
+{
+	if ( _islower(c) == 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
