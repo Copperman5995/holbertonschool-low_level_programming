fbc9a35ae8815e8c3d7640dc74a0b75553ce9bd7
root <cooper.d.mix@gmail.com>
2023-01-30 11:06:34 -0800

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,11 @@
 #include "main.h"
 /**
  * print_last_digit - pritns last digit of number
- *
- * Return: always 0
+ *@c: c is a interager
+ * Return: return the last digit of c
  */
-int print_last_digit(int)
+int print_last_digit(int c)
 {
+	int ld = c % 10;
+	return (ld);
+}
