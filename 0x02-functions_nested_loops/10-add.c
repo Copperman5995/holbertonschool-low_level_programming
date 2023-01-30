1629b2c452aeab95af8090660b5585b06a895db6
root <cooper.d.mix@gmail.com>
2023-01-30 13:14:37 -0800

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * add - adds int a to int b 
+ * add - adds int a to int b
  * @a: first int
  * @b: second int
  * Return: returns the result of int a plus int b
@@ -8,6 +8,6 @@
 int add(int a, int b)
 {
 	int r = a + b;
-	
+
 	return (r);
 }
