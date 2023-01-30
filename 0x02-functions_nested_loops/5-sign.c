ea02439a8126506f338a6ffc20c1e95e2760f4ee
root <cooper.d.mix@gmail.com>
2023-01-30 10:22:13 -0800

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
-#include "main.h"
 /**
  * print_sign - determisn if c is pos or neg then returns sing
  * @n: is a int
  * Return: returns 1 and prints + is n is grater than 0, returns 0 and prints 0 is n is 0, returns -1 and prints - if n is less than 0
  */
+#include "main.h"
 int print_sign(int n)
 {
 	if (n > 0)
@@ -16,7 +16,7 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	else if (n == 0)
+	else
 	{
 		_putchar('0');
 		return (0);
