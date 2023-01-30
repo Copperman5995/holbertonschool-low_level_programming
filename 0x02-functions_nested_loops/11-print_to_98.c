dbb213687e8d39b4191978a95b5ad05f07fd0ea5
root <cooper.d.mix@gmail.com>
2023-01-30 13:37:50 -0800

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n in 98
+ * @n: is starting int
+ * Return: returns 0
+ */
+void pritn_to_98(int n)
+{
+	int to = n;
+	
+	if (n > 98)
+	{
+		while (to >= 98)
+		{
+			if (to >= 10)
+			{
+				_putchar((to / 10) + '0');
+				_putchar((to % 10) + '0');
+			}
+			else
+			{
+				_putchar(to + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+			to--
+		}
+	}
+	else if (n < 98)
+	{
+		while (to <= 98)
+		{
+			if ( to >= 10)
+			{
+				_putchar((to / 10) + '0');
+				_putchar((to % 10) + '0');
+			}
+			else
+			{
+				_putchar(to + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+			to++
+		}
+	}
+	else if (n == 98)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+}
