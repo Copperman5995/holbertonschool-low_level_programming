2fee71a813213d2d0bea531f17adbb8bd76252fc
root <cooper.d.mix@gmail.com>
2023-01-30 13:11:47 -0800

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * add - adds int a to int b 
+ * @a: first int
+ * @b: second int
+ * Return: returns the result of int a plus int b
+ */
+int add(int a, int b)
+{
+	int r = a + b;
+	if ( x >= 10)
+	{
+		_putchar((r / 10) + '0');
+		_putchar((r % 10) + '0');
+	}
+	else
+	{
+		_putchar(r + '0');
+	}
+	return (r);
+}
