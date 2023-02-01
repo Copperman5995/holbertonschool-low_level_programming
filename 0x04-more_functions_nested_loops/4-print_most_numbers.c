fa19dbfb335cad2b06f16b51a35606c4a7a09d19
root <cooper.d.mix@gmail.com>
2023-02-01 09:57:17 -0800

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers 0 - 9
+ *
+ * Return: returns nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if ( i != 2 || i != 4)
+		{
+			_putchar(i + '0');
+		}
+		i++;
+	}
+	_putchar('\n');
+}
