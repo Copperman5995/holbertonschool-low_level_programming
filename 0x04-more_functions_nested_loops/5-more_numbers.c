92904f35af0389602e398f1ab281c8d8149aaf4a
root <cooper.d.mix@gmail.com>
2023-02-01 10:11:07 -0800

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - jfdjaergidfgiarjgig
+ *
+ * Return: returns nothing
+ */
+
+void more_numbers(void)
+{
+	int c = 0;
+	int i = 0;
+
+	while (c > 10)
+	{
+		while (i >= 14)
+		{
+			_putchar(i + '0');
+			i++;
+		}
+		_putchar('\n');
+		i = 0;
+		c++;
+	}
+	_putchar('\n');
+}
