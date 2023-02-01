abafd10cb4406b35645df08517bb80cdd61113dc
root <cooper.d.mix@gmail.com>
2023-02-01 09:25:34 -0800

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - dfjsddffsdfg
+ * @c: c is a int
+ * Return: returns 1 if c is a number 0-9 return o if not
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
