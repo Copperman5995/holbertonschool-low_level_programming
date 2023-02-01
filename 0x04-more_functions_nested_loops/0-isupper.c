f3bff7dbfb91d3c3559d6688614c8c811187c0de
root <cooper.d.mix@gmail.com>
2023-02-01 09:18:26 -0800

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - ghghgjgjdjdjdjddjjd
+ * @c: c is a ASCII chater
+ * Returns: return 1 if c is uppercase if not return 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
