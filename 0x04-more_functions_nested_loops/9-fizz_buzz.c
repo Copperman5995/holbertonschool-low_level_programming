fbe4a40c8654a27bc25fd96b06f9182f637adcd7
root <cooper.d.mix@gmail.com>
2023-02-02 09:53:22 -0800

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - jfjjgjjgjgjgjjgjg
+ *
+ * Return: retursn nothing
+ */
+
+int main(void)
+{
+	int x = 1;
+
+	while ( x <= 100)
+	{
+		if ((x % 3) == 0 && (x % 5) == 0)
+		{
+			printf("Fizzbuzz");
+		}
+		else if ((x % 3) == 0 && (x % 5) != 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((x % 3) != 0 && (x % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+		x++;
+	}
+}
+
