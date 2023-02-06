3e19640523bffa40a07c3c16c469e1b92bec6de8
root <cooper.d.mix@gmail.com>
2023-02-06 12:36:32 -0800

    8

---
+++ 0x05-pointers_arrays_strings/8-print-array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - gkadfjgkjdfkjdfkjhjds
+ * @a: dfkgkjsdkfjhsdgh
+ * @n: kdghljsdgjjhsdjg
+ * Return: aldgjkjdgsdkjgtjd
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (a[i] <= n)
+	{
+		_putchar(s[i]);
+		printf(", ");
+		i++;
+	}
+	_putchar('\n');
+}
