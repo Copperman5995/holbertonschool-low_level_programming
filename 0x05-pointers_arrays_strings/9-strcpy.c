bb3950650a027b8c06c233eeb5b1e6f22658cdbd
root <cooper.d.mix@gmail.com>
2023-02-06 14:04:55 -0800

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy - djkfgjshdfjglk
+ * @dest: jdfhgjhd
+ * @src: kdfjgldjfg
+ * Return: returns pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++
+	}
+	src[i] = '\n';
+	return (dest);
+}
