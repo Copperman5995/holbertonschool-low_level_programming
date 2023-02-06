c409bfb18ff5ffacc75927a440f90fdce636beca
root <cooper.d.mix@gmail.com>
2023-02-06 12:10:10 -0800

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,3 +5,17 @@
  * Return: dkfgjjdfgjhdf
  */
 
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++
+	}
+	_putchar('\n');
+}
