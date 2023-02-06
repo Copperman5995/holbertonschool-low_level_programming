b16a55b50069bc56a1f013fdff015d6805d9b930
root <cooper.d.mix@gmail.com>
2023-02-06 11:30:17 -0800

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_rev - gggggggggggggg
+ *@s: ggggggggggggggg
+ *Return: ggggggggggggggggggggg
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (; s[i] >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
+
