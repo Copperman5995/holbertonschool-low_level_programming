b8e906f6480d94683cbb9009dcf5773594e6ba86
root <cooper.d.mix@gmail.com>
2023-02-06 11:15:07 -0800

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,2 +1,19 @@
 #include "main.h"
+/**
+ * _strlen - ajdfsgdfjjgjfdjfjgskdfk
+ * @s: gggghdsgagerget
+ * Return: returns int
+ */
+
+int _strlen(char *s)
+{
+	char l[] = (char)*s;
+
+	int i;
+
+	for (i = 0; l[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
 
