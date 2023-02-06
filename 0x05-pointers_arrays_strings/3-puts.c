ec8582be305cf97db61bb979676959b9576ae5af
root <cooper.d.mix@gmail.com>
2023-02-06 11:23:42 -0800

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,3 +1,18 @@
 #include "main.h"
 /**
- *
+ *_puts - gggggggggggggg
+ *@str: ggggggggggggggg
+ *Retur: ggggggggggggggggggggg
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
+
+
