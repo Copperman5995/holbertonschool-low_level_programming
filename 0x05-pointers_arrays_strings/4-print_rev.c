477b647218b0b4ea497c4f9d4b6dc93d332f9f17
root <cooper.d.mix@gmail.com>
2023-02-06 11:40:50 -0800

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,14 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	for (; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
+		i++;
 	}
-	for (i = i - 1; s[i] >= 0 ; i--)
+	while (s[i] >= 0)
 	{
 		_putchar(s[i]);
+		i--;
 	}
 
 	_putchar('\n');
