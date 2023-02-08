ae231042302e014d73c03af894e5b936b328a203
root <cooper.d.mix@gmail.com>
2023-02-08 12:55:50 -0800

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,6 +9,8 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int srclen = _strlen(src);
+	int destlen = _strlen(dest);
 	int c = 0;
 
 	while (c <= (n - 1))
@@ -16,10 +18,35 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[c] = src[c];
 		c++;
 	}
+	if (srclen < n)
+	{
+		while (c <= destlen)
+		{
+			dest[c] = '\0';
+			c++;
+		}
+	}
 	return (dest);
 
 
 
 
 
+}
+
+/**
+ * _strlen - sdkgjjfgifdsf
+ * @s: dfkgkjdkfjg
+ * Return: dkfgkjdfj
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++
+	}
+	return (i);
 }
