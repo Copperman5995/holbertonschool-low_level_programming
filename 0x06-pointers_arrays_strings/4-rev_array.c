5053323b3e5d5f348565adaf9539acfe2b4e10eb
root <cooper.d.mix@gmail.com>
2023-02-09 09:51:14 -0800

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,12 +12,12 @@ void reverse_array(int *a, int n)
 	int c1 = 0;
 	int c2 = 0;
 
-	while (a[c1] != '\0')
+	while (c1 < n)
 	{
 		temp[c1] = a[c1];
 		c1 ++
 	}
-
+	c1 = c1 - 1;
 	while (c2 < n)
 	{
 		a[c2] = temp[c1];
