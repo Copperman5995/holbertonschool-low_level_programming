678ebdac19cf41462f18cdd1c94f83604aa6f9d3
root <cooper.d.mix@gmail.com>
2023-02-09 09:10:45 -0800

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,33 +8,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int c1 = 0;
-	int c2 = 0;
-	int t1 = 0;
-	int t2 = 0;
-
-	while (s1[c1] != '\0')
+	while (s1 && s2 && s1 == s2)
 	{
-		t1 = t1 + s1[c1];
-		c1++;
+		s1++;
+		s2++
 	}
-
-	while (s2[c2] != '\0')
-	{
-		t2 = t2 + s2[c2];
-		c2++;
-	}
-
-	if (t1 < t2)
-	{
-		return (-1);
-	}
-	else if (t1 > t2)
-	{
-		return (1);
-	}
-	else if (t1 == t2)
-	{
-		return (0);
-	}
+	return (s1 - s2);
 }
