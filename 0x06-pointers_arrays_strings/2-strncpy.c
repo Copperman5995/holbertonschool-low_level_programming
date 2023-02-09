8c080c7b5871d18712ce4bd570e0ba8c527d0255
root <cooper.d.mix@gmail.com>
2023-02-09 08:33:00 -0800

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int destlen = _strlen(dest);
 	int c = 0;
 
-	while (src[c] != '\0' && srclen < n)
+	while (src[c] != '\0' && srclen > n)
 	{
 		dest[c] = src[c];
 		c++;
