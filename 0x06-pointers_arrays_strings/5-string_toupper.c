c9a0cf1d66c7922d9a446fdeb1efba7c26ede9df
root <cooper.d.mix@gmail.com>
2023-02-09 12:32:54 -0800

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * string_toupper - changes lowercse letter to uppercase
+ * @str: akdsfgkldfg
+ * Return: ksjdgkldkjf
+ */
+
+char *string_toupper(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		if (str[len] >= 'a' && str[len] <= 'z')
+		{
+			str[len] = str[len] -32;
+		}
+		len++;
+	}
+	return (str);
+}
