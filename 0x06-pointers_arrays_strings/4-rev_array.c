4a933fe681839e9a5fadc8c5dcd6f7de3fa39c1a
root <cooper.d.mix@gmail.com>
2023-02-09 09:41:54 -0800

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * reverse_array - kdjfkgjdkfjg
+ * @a: dfgjhdfkjdjfru
+ * @n: ldfjgkdfijgdf
+ * Return: aksdfgdjfkd
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp[];
+	int c1 = 0;
+	int c2 = 0;
+
+	while (a[c1] != '\0')
+	{
+		temp[c1] = a[c1];
+		c1 ++
+	}
+
+	while (c2 < n)
+	{
+		a[c2] = temp[c1];
+		c2++;
+		c1--:
+	}
+}
+
