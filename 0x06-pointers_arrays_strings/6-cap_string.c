9c2c6a001f1330af5ce6c8cdcf171790df98a265
root <cooper.d.mix@gmail.com>
2023-02-09 19:45:06 -0800

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,59 +7,59 @@
 
 char *cap_string(char *str)
 {
-    int index = 0;
+	int index = 0;
 
-    while (str[index])
-    {
-        if (index == 0)
-        {
-            if (str[index] >= 'a' && str[index] <= 'z')
-            {
-                str[index] = str[index] - 32;
-            }
-        }
-        if (str[index + 1] >= 'a' && str[index + 1] <= 'z')
-        {
-        if (str[index] == ' ' || str[index] == '\t')
-        {
-            str[index + 1] = str[index + 1] - 32;
-        }
-        else if (str[index] == '\n' || str[index] == ',')
-        {
-            str[index + 1] = str[index + 1] - 32;
-        }
-        else if (str[index] == ';' || str[index] == '.')
-        {
-            str[index + 1] = str[index + 1] - 32;
-        }
-        else if (str[index] == '!' || str[index] == '?')
-        {
-            str[index + 1] = str[index + 1] - 32;
-        }
-        else if (str[index] == '"' || str[index] == '(')
-        {
-            str[index + 1] = str[index + 1] - 32;
-        }
-        else if (str[index] == ')' || str[index] == '{')
-        {
-            str[index + 1] = str[index + 1] - 32;
-        }
-        else if (str[index] == '}')
-        {
-            str[index + 1] = str[index + 1] - 32;
-        }
-        else if (!(str[index] >= 'a' && str[index] <= 'z'))
-        {
-            if (!(str[index + 1] >= 'A' && str[index + 1] <= 'Z'))
-            {
-            }
-            else
-            {
-                str[index + 1] = str[index + 1] - 32;
-            }
-        }
-        }
-        index++;
-    }
-    return (str);
+	while (str[index])
+	{
+		if (index == 0)
+		{
+			if (str[index] >= 'a' && str[index] <= 'z')
+			{
+				str[index] = str[index] - 32;
+			}
+		}
+		if (str[index + 1] >= 'a' && str[index + 1] <= 'z')
+		{
+			if (str[index] == ' ' || str[index] == '\t')
+			{
+				str[index + 1] = str[index + 1] - 32;
+			}
+			else if (str[index] == '\n' || str[index] == ',')
+			{
+				str[index + 1] = str[index + 1] - 32;
+			}
+			else if (str[index] == ';' || str[index] == '.')
+			{
+			str[index + 1] = str[index + 1] - 32;
+			}
+			else if (str[index] == '!' || str[index] == '?')
+			{
+				str[index + 1] = str[index + 1] - 32;
+			}
+			else if (str[index] == '"' || str[index] == '(')
+			{
+				str[index + 1] = str[index + 1] - 32;
+			}
+			else if (str[index] == ')' || str[index] == '{')
+			{
+				str[index + 1] = str[index + 1] - 32;
+			}
+			else if (str[index] == '}')
+			{
+				str[index + 1] = str[index + 1] - 32;
+			}
+			else if (!(str[index] >= 'a' && str[index] <= 'z'))
+			{
+				if (!(str[index + 1] >= 'A' && str[index + 1] <= 'Z'))
+				{
+				}
+				else
+				{
+					str[index + 1] = str[index + 1] - 32;
+				}
+			}
+		}
+		index++;
+	}
+	return (str);
 }
