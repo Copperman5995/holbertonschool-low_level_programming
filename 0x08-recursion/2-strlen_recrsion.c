7d25fead76d826284d28147c1b16e99bed296b56
root <cooper.d.mix@gmail.com>
2023-02-15 14:29:19 -0800

    2

---
+++ 0x08-recursion/2-strlen_recrsion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strlen_recusrion - returns lenghts of string using recusion
+ * @s: the string
+ * Returns the length of the string
+ */
+
+int _strlen_recusrion(char *s)
+{
+	static int len = 0;
+
+	if (*s != '\0')
+	{
+		length++;
+		_strlen_recursion(++s);
+	}
+	else
+	{
+		return (len);
+	}
+}
+	
