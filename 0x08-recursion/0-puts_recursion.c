909fb507559c7ccf6a07695dbaaa03fc6677ef6d
root <cooper.d.mix@gmail.com>
2023-02-15 13:45:23 -0800

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string using recursion
+ * @s: string that is being printed
+ * Return: returns nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	int index = 0;
+
+	while (s[index])
+	{
+		_putchar(s[index]);
+		index++;
+	}
+}
