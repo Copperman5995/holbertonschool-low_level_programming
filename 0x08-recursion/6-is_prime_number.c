f9aa832faaeccf3d61417ea6aab63b403210f658
root <cooper.d.mix@gmail.com>
2023-02-17 10:38:51 -0800

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,16 +7,15 @@
 
 int is_prime_number(int n)
 {
-	int c;
 	
 	if (n == 0 || n == 1)
 	{
 		return (0);
 	}
 
-	if (n % 1 == 0 && n % n = 0 && n % (n / 2) != 0)
+	if (n % 1 == 0 && n % n == 0 && n % (n / 2) != 0)
 	{
 		return (1);
 	}
-	
+	return (0);
 }
