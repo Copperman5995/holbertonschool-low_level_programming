99359c1cde7d7b694312509c6fa6411b44905147
root <cooper.d.mix@gmail.com>
2023-02-17 10:05:53 -0800

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - dfigsdfjigad
+ * @x: djghjfg
+ * @y: sdjfgjdfg
+ * Return:returns x to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
+		
