982d0201880342501b6456180f2020b2b1ef2358
root <cooper.d.mix@gmail.com>
2023-02-17 10:33:21 -0800

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,5 +4,5 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sprt_recursion(int n);
-int is_prime_number(int n);
+int is_prime_number(int n, int i);
 
