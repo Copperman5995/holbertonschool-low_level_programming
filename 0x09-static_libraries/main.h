568cf372817105acdd225948940fb8b68050c976
root <cooper.d.mix@gmail.com>
2023-02-17 14:06:00 -0800

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,4 @@
-#ifndef _putchar
-#define _putchar
+#include <stdio>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +19,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
