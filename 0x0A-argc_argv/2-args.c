a3b139e630b5ab01df9ad89b974a8da8c6284ff5
root <cooper.d.mix@gmail.com>
2023-02-17 17:49:04 -0800

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - sdfdsfs
+ * @argc: sdfgdsf
+ * @argv: dfgdfg
+ * Return: sdfgd
+ */
+
+int main(int argc, int *argv[])
+{
+	int i = 0;
+
+	while (argv[i])
+	{
+		printf("%s", argv[i]);
+		i++;
+	}
+	return (0);
+}
