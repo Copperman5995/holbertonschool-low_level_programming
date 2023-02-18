202ac40d443246624ea89c13d8b17e015721ba52
root <cooper.d.mix@gmail.com>
2023-02-17 17:18:45 -0800

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - sjgkgs
+ * @argc: sdjk
+ * @argv: sjkgkd
+ * Return: kdfjg
+ */
+
+int main(int a*rgv[])
+{
+	printf("%c", argv[0]);
+	return (0);
+}
