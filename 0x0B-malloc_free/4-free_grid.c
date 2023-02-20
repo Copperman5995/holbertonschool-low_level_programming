39b196835425e82a3bf7ff5295713746a2ce23d7
root <cooper.d.mix@gmail.com>
2023-02-20 15:26:48 -0800

    4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - sdfgdfg
+ * @grid: safdgsd
+ * @height: sdfge
+ * Return: sdfgsdg
+ */
+
+void free_grid(int **grid, int height)
+{
+	int c;
+
+	for (c = 0; c <= height; c++)
+	{
+		free(grid[c]);
+	}
+}
