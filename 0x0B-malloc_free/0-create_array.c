92c0f365c0043de34ff27abc1ac7a1933c0d8890
root <cooper.d.mix@gmail.com>
2023-02-20 11:14:24 -0800

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,10 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return ("NULL");
+		return ('\0');
 	}
 
-	new_array = (char*)malloc(size * sizeof(char));
+	new_array = (char *)malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 	{
