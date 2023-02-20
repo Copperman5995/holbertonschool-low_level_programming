d94ef07038bc561ccf5443b3020afeab275a4cd2
Copperman5995 <118470269+Copperman5995@users.noreply.github.com>
2023-02-20 14:34:29 -0600

    main.c

--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -5,8 +5,12 @@
 int main(void)
 {
     char *s;
+    char *s2;
+    char *s3;
 
-    s = str_concat("Best ", NULL);
+    s = str_concat("Hello", NULL);
+    s2 = str_concat(NULL, "Hello");
+    s3 = str_concat(NULL, NULL);
     if (s == NULL)
     {
         printf("failed\n");
