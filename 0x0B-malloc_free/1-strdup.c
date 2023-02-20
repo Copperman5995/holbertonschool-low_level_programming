a52347d510be1a303b8b9890772131d743103f44
root <cooper.d.mix@gmail.com>
2023-02-20 11:49:23 -0800

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - sdfgdsfg
+ * @str: dsfgdsfg
+ * Return: dfgsdfgdsf
+ */
+
+char *_strdup(char *str);
+{
+	char *new_array;
+	int index1 = 0;
+	int index2 = 0;
+
+	if(str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[index1])
+	{
+		index1++;
+	}
+	new_array= malloc(index * sizeof(char));
+
+	while (index2 <= index1)
+	{
+		new_array[index2] = str[index2];
+		index2++;
+	}
+	return (new_array);
+}
