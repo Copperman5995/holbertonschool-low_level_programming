c5ee9d70ab804cdba413b1f00d635c9c452ce76a
root <cooper.d.mix@gmail.com>
2023-02-20 12:18:31 -0800

    2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - sdfgsadgdaf
+ * @s1: dfgdsf
+ * @s2: sdfgfd
+ * Return: sdfgdsfg
+ */ 
+
+char *str_concat(char *s1, char *s2)
+{
+	int len1 = 0;
+	int len2 = 0;
+	int index = 0;
+	char *new_array;
+
+
+
+	while (s1[len1])
+	{
+		len1++;
+		index++;
+	}
+
+	while (s2[len2])
+	{
+		len2++;
+		index++;
+	}
+
+	new_array = malloc(index * sizeof(char));
+
+	if (new_array == NULL)
+	{
+		return (NULL);
+	}
+
+	len1 = 0;
+	len2 = 0;
+	index = 0;
+
+	while (s1[len1])
+	{
+		new_array[index] = s1[len1];
+		len1++;
+		index++;
+	}
+	while (s2[len2])
+	{
+		new_array[index] = s2[len2];
+		len2++;
+		index++;
+	}
+	return (new_array);
+}
+
+
