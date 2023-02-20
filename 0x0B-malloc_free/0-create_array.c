c46580b646aa692985a14a6b8fa706f598805621
root <cooper.d.mix@gmail.com>
2023-02-20 11:20:58 -0800

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,6 +21,11 @@ char *create_array(unsigned int size, char c)
 		new_array = malloc(size * sizeof(char));
 	}
 
+	if (s == NUL)
+	{
+		return ('\0');
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		new_array[i] = c;
