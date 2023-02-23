841046f1d0eb535f0257e3d5e9f1dbbf73ed7c88
root <cooper.d.mix@gmail.com>
2023-02-23 13:59:55 -0800

    4

--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
+#include "4-SUM.h"
+#include "4-SUM.h"
 int main(void)
 {
     int i;
-    int j;
 
-    i = ABS(-98) * 10;
-    j = ABS(98) * 10;
-    printf("%d, %d\n", i, j);
+    i = SUM(98,1024);
+    printf("%d\n", i);
     return (0);
 }
