ad1b7a7e48d97bdf6a5ae08bed6c6ed1542628b9
root <cooper.d.mix@gmail.com>
2023-03-02 11:27:00 -0800

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * new_dog - sdfgsdf
+ * @name: asdf
+ * @age: sdfgdsg
+ * @owner: sdfgsdfg
+ * Return: sdfgsdfg
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
