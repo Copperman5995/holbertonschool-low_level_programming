74f37e3db9777f7a92197ea9e9a70d53a2880585
root <cooper.d.mix@gmail.com>
2023-03-02 10:06:12 -0800

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,29 +12,29 @@ void print_dog(struct dog *d)
 	{
 		if ((*d).name != NULL)
 		{
-			printf("Name: %s", (*d).name);
+			printf("Name: %s\n", (*d).name);
 		}
 		else
 		{
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 		}
 
-		if ((*d).age != NULL)
+		if (sizeof((*d).age) > 0)
 		{
-			printf("Age: %.1f", (*d).age);
+			printf("Age: %.1f\n", (*d).age);
 		}
 		else
 		{
-			printf("Age: (nil)");
+			printf("Age: (nil)\n");
 		}
 
 		if ((*d).owner != NULL)
 		{
-			printf("Owner: %s", (*d).owner);
+			printf("Owner: %s\n", (*d).owner);
 		}
 		else
 		{
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 		}
 	}
 }
