2f2282d7c0aaf1dbcbad8e78c396d33d29484ed9
root <cooper.d.mix@gmail.com>
2023-03-02 09:25:00 -0800

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - sdfgdsg
+ * @name: asdfgasdf
+ * @age: asdfef
+ * @owner: asdfasdf
+ * Description: asdfasdfdsa
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
