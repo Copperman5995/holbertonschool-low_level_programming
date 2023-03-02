38317173ebd4122c720fbf4d346c5eda66ae6dcd
root <cooper.d.mix@gmail.com>
2023-03-02 11:16:59 -0800

    dog.h

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -2,11 +2,11 @@
 #include "dog.h"
 int main (void)
 {
-	struct dog my_dog;
+	struct dog_t my_dog;
 
 	my_dog.name = "poopy";
 	my_dog.age = 3.5;
 	my_dog.owner = "boob";
-	print_dog(&my_dog);
+	printf("my name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 	return (0);
 }
