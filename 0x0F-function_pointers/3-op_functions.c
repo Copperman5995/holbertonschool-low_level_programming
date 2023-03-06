67b52f9352e700b959fded78441c9f69401d7527
root <cooper.d.mix@gmail.com>
2023-03-06 11:59:39 -0800

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - adds int a and int b
+ * @a: first int
+ * @b: second int
+ * Return: returns that sum of a and b
+ */
+int op_add(int a, int b)
+{
+	int i = a + b;
+
+	return (i);
+}
+/**
+ * op_sub - subtracts a from b
+ * @a: first int
+ * @b: second int
+ * Return: retruns difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	int i = a - b;
+
+	return (i);
+}
