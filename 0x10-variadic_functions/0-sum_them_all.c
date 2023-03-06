0e6ce6bf1364252f19871b96e217136ea855229c
root <cooper.d.mix@gmail.com>
2023-03-06 14:55:00 -0800

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * sum_them_all - adds all the together
+ * @n: is the number of ints that will be added
+ * Return: returns the sum of the ints
+ */
+
+int sum_them_all (const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i = 0;
+
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	while (i <= n)
+	{
+		sum += ca_arg(ptr, int);
+		i++;
+	}
+	va_end(ptr);
+	 
+	return (sum);
+}
