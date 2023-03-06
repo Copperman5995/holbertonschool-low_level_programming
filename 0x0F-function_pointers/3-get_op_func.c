978f29216d43c45fa2ec14dbe511d40d6a83773a
root <cooper.d.mix@gmail.com>
2023-03-06 13:04:44 -0800

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,21 +1,5 @@
 #include "3-calc.h"
 /**
- * slen - finds string lenght
- * @str: string
- * Return: returns the lenght of str
- */
-
-int slen(char *str)
-{
-	int len = 0;
-
-	while (str)
-	{
-		len++;
-	}
-	return (len);
-}
-/**
  * get_op_func - selects the correct function for the operation selected
  * @s: is the operator passed
  * Return: returns something
@@ -34,7 +18,7 @@ int (*get_op_func(char *s))(int, int)
 	int i = 0;
 
 
-	if (slen(s) != 1)
+	if (strlen(s) != 1)
 		return (NULL);
 
 	while (ops[i].op != NULL || ops[i].op != s)
