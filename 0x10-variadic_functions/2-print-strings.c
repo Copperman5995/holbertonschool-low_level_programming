c6395f097fa0b9c56da59ea10f1d80ef2e0ebdd8
root <cooper.d.mix@gmail.com>
2023-03-06 20:03:38 -0800

    2

---
+++ 0x10-variadic_functions/2-print-strings.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints words
+ * @separator: sdfsdf
+ * @n: sdfgsdfg
+ * Return: sdfger
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *s;
+	va_list yeet;
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(yeet, n);
+
+	while (i < n)
+	{
+		s = va_arg(ap, char *);
+		if (s == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", s);
+		}
+		if (separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	s = va_arg(yeet, char *);
+	if (s == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("%s\n", s);
+	}
