b2722acbdb051fbb56ec64ce88fb68b517de590b
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-15 09:52:03 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
 size_t print_list(const list_t *h)
 { 
 	int num = 0;
-	const list_t *temp = h
+	const list_t *temp = h;
 
 	while (temp != NULL)
 	{
@@ -17,9 +17,12 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		}
 		else
+		{
 		printf("[%d] %s\n", temp->len, temp->str);
+		}
+		num++;		
 		temp = temp->next;
-		num++;
+		
 	}
 	return (num);
 
