8a1da9620164a528dbe610a873fc5a59c5a82f6a
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-20 13:11:35 -0700

    3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_node_end - sdfg
+ * @head: asfgd
+ * @str: sdfgsdf
+ * Return: sdfgdf
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *addnode = malloc(sizeof(list_t));
+	list_t *temp = *head
+
+	if (!addnode)
+		return (NULL);
+
+	addnode->str = strdup(str);
+	addnode->len = strlen(str);
+	addnode->next = NULL;
+
+	if (*head == NULL)
+	{
+	       *head = addnode;
+	}
+	else
+	{
+		while (temp->next)
+		{
+			temp = temp->temp;
+		}
+		temp->next = addnode;
+	}
+	return (addnode);
+}
+
