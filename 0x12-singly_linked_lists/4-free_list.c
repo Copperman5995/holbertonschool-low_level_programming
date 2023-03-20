67548a5c79d864dc49a0eab9909f267ec304ecc7
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-20 13:40:59 -0700

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - sdgsdg
+ * @head: adfgsdf
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	return (head);
+}
+
