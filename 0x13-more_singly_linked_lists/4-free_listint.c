e3fc767e1bb6527c07706609311979988e4fa644
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-20 17:20:57 -0700

    4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - sdgsdg
+ * @head: adfgsdf
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
+
