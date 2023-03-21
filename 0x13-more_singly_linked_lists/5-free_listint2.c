b2b3bba7449b495c286770ef47473fe9e35bad6b
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-20 17:46:56 -0700

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- * free_listint - sdgsdg
+ * free_listint2 - sdgsdg
  * @head: adfgsdf
  */
-void free_listint(listint_t *head)
+void free_listint2(listint_t *head)
 {
 	listint_t *temp;
 
