0ffd1d1608d69f890dcffd5287dc209804728f71
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-20 19:26:24 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_binary - adfgsdfg
+ * @n: sd gsdfg
+ * Return: sdfgd
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar(n - 2 * (n >> 1) + '0');
+		return;
+	}
+
+	print_binary(n >> 1);
+
+	_putchar(n - 2 * (n >> 1) + '0');
+}
