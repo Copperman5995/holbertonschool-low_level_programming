8582a2afb6bf0f14e3e8574067ad132f634721f6
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-20 19:11:22 -0700

    _pow

---
+++ 0x14-bit_manipulation/_pow.c
@@ -0,0 +1,24 @@
+/**
+ * _pow - sdfgdfg
+ * @i: sdfgdsfg 
+ * @b: asdfgsdfg
+ * Return: sdfgds
+ */
+unsigned int _pow(unsigned int i, unsigned int b)
+{
+	unsigned int num = i;
+	unsigned int c = 1;
+
+	if (b == 0)
+	{
+		return (1);
+	}
+
+	while (c < b)
+	{
+		num = num * a;
+		c++;
+	}
+
+	return (num);
+}
