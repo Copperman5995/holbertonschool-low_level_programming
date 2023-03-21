b6b207132a61de3754df3b55ecd72160fd99db9d
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-20 17:42:59 -0700

    7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - asdfgsdfg
+ * @head: adfgsdfg
+ * @index: asdfgas
+ * Return: sdfgsdfg
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint+t *temp = head;
+	unsigned int indexPrint = 0;
+
+	while (temp)
+	{
+		if (indexPrint == index)
+		{
+			return (temp);
+		}
+		temp =temp->next;
+		indexPrint++;
+	}
+	return (NULL);
+}
+
