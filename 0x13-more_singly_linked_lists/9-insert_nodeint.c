d88c9c3b4e0619b9bd1c1f190f04911e1f2457d4
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-20 18:18:56 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		insertnode->next = *head;
+		insertNode->next = *head;
 		*head = insertNode;
 		return (insertNode);
 	}
