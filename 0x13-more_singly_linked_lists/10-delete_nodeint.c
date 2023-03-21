a6e295f55cf2c3a1a0f6b987b629779769209238
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-20 18:29:53 -0700

    10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		delNode = *head;
-		*head -= (*head)->next;
+		*head = (*head)->next;
 		free(delNode);
 		return (1);
 	}
@@ -41,8 +41,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	}
 
-	nodeDel = temp->next;
+	delNode = temp->next;
 	temp->next = temp->next->next;
-	free(nodeDel);
+	free(delNode);
 	return (1);
 }
