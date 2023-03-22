de2cd6042c519da1aa3288fa133c0b5ff21a6488
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-22 13:26:59 -0700

    2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.c"
+/**
+ * get_bit - sdfgdfg
+ * @n: sdfgdf
+ * @index: sdfgdfg
+ * Return: sdfgdfg
+ */
+
+int get_bit(unsigned long it n, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (n <= 1)
+	{
+		return (n % 2);
+	}
+
+	while (n >= 1)
+	{
+
