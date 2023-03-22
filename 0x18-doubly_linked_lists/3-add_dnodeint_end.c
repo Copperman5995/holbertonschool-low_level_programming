b0d994d43bc599d6227691179f8b79bcf3e33df1
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-22 08:56:27 -0700

    3

---
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * add_dnodeint _end - sdfgdsf
+ * @head: sdfgdf
+ * @n: adsfgsdfg
+ * Return: sdfgdfgdfg
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlsitint_t *temp = *head;
+	dlistint_t *newNodeEnd = malloc(sizeof(newNodeEnd));
+
+	if (!newNodeEnd)
+	{
+		return (NULL);
+	}
+
+	newNodeEnd->n = n;
+	newNodeEnd->next = NULL;
+
+	if (!*head)
+	{
+		*head = newNodeEnd;
+		return (newNodeEnd);
+	}
+
+	while (temp->next)
+	{
+		temp = temp->next;
+	}
+
+	temp->next = newNodeEnd;
+	
+	return (newNodeEnd);
+}
