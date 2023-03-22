5d53d7f9c6c21bf2cf3c6425c3f93b8033f78866
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-22 08:33:49 -0700

    2

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -19,8 +19,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	newNodeBegin->n = n;
 	newNodeBegin->next = temp;
-	*head = newNodeBegin;
 	newNodeBegin->prev = NULL;
+	*head = newNodeBegin;
 
 	return (newNodeBegin);
 }
