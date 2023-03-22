fdd50e10a2ca17ae342a7ade0ab2d6018abc5b8d
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-22 13:55:40 -0700

    5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * flip_bits - sdfgdfg
+ * @n: sdfgdfsg
+ * @m: sdfgdf
+ * Return: sdfgdfg
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int flip = 0;
+	unsigned long int yeet = n ^ m;
+
+	while (yeet > 0)
+	{
+		fipl++;
+		yeet &= (num -1);
+	}
+
+	return (flip);
+}
