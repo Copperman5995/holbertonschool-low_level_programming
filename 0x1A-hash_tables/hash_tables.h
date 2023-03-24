45d2eb018c62abb47587078cd67d6704e763454b
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-24 12:49:46 -0700

    hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,9 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 /**
  * struct has_node_s - Node of a hash table
  *
