3f7812f972e2f9ffd721093219dd36a24c2f78bd
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-24 11:52:59 -0700

    8

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -28,7 +28,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (temp && index != 0)
+	while (temp && indexDel < index - 1)
 	{
 		if (indexDel == index - 1)
 			break;
