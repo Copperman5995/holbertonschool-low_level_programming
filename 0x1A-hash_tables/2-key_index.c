451bbb3d17b678c7690d4ba354b349a62daa9caa
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-27 09:18:02 -0700

    2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * key_index - sdfgdsfgdfsg
+ * @key: sdfgsdfg
+ * @size: asdfgdsfg
+ * Return:  sdfgsd
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int idex;
+
+	idex = hash_djb2(key);
+	return (index % size);
+}
